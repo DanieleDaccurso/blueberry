86894a21fa0d61b39b15f1617f02eabf5f6aab74
daniele <daniele@danieles-thinkpad.lh>
2017-12-25 15:41:19 +0100

    app refactor

--- blueberry.go
+++ blueberry.go
@@ -1,18 +1,33 @@
 package blueberry
 
 import (
-	"github.com/danieledaccurso/blueberry/router"
 	"github.com/danieledaccurso/blueberry/glue"
 	"github.com/danieledaccurso/blueberry/headless"
+	"github.com/danieledaccurso/blueberry/router"
+	"net/http"
+	"fmt"
 )
 
+type AppConfig struct {
+	Server struct {
+		TLS struct {
+			Enabled  bool
+			CertFile string
+			KeyFile  string
+		}
+		ListenHost string
+	}
+}
+
 type App struct {
-	Router *router.Router
+	Router    *router.Router
+	AppConfig *AppConfig
 }
 
 func New() *App {
 	a := new(App)
 	a.Router = router.NewRouter()
+	a.AppConfig = new(AppConfig)
 	return a
 }
 
@@ -21,7 +36,16 @@ func (a *App) WithJsonResponse() *App {
 	return a
 }
 
-func (a *App) WithCORSEnables() *App {
+func (a *App) WithCORSEnabled() *App {
 	a.Router.AppendPreRequestEvent(new(headless.CORSEvent))
 	return a
+}
+
+func (a *App) ListenAndServe() {
+	if !a.AppConfig.Server.TLS.Enabled {
+		err := http.ListenAndServe(a.AppConfig.Server.ListenHost, a.Router)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
 }
